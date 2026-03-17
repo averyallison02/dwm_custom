73e0272389db2f0f1a986f9b66274e8cd703c482
Avery Allison <averyallison02@gmail.com>
2026-03-16 17:09:29 -0700

    added clock functionality to xroot

--- dwm_autostart.sh
+++ dwm_autostart.sh
@@ -1,2 +1,7 @@
 # dwm will not start until the shell returns from running this script.
 # note that you should still background any processes which should run at the same time as others.
+
+while true; do
+    xsetroot -name "$( date +"%a %b %d %Y %I:%M %p" )"
+    sleep "$(expr 60 - $( date +"%S" ) )s"
+done &
