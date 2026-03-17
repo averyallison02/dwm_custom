82744e1d2b97a51e89824658a662aba54ae3f855
Avery Allison <averyallison02@gmail.com>
2026-03-16 18:30:20 -0700

    added picom and desktop background to autostart

--- dwm_autostart.sh
+++ dwm_autostart.sh
@@ -1,7 +1,14 @@
 # dwm will not start until the shell returns from running this script.
 # note that you should still background any processes which should run at the same time as others.
 
+# update clock when necessary
 while true; do
     xsetroot -name "$( date +"%a %b %d %Y %I:%M %p" )"
     sleep "$(expr 60 - $( date +"%S" ) )s"
 done &
+
+# start compositor
+picom &
+
+# set desktop background
+feh --bg-fill "$HOME/backgrounds/torrent.jpg"
