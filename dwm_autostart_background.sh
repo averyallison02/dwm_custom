d65c6734eedd6020fd7e1205200741af4be84ce9
Avery Allison <averyallison02@gmail.com>
2026-03-27 16:26:16 -0700

    made autostart applications silent

--- dwm_autostart_background.sh
+++ dwm_autostart_background.sh
@@ -1,6 +1,6 @@
 # dwm will start regardless of the status of this script.
 # note that you should still background any processes which should run at the same time as others.
 
-discord &
+discord --start-minimized &
 
-steam &
+steam -silent &
