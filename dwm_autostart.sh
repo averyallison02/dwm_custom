8024c4add52cc2cc3d9b060151d489cff0f7215d
Avery Allison <averyallison02@gmail.com>
2026-04-09 14:43:25 -0700

    added battery readout to root loop

--- dwm_autostart.sh
+++ dwm_autostart.sh
@@ -1,10 +1,12 @@
 # dwm will not start until the shell returns from running this script.
 # note that you should still background any processes which should run at the same time as others.
 
-# update clock when necessary
+# update changing information
 while true; do
-    xsetroot -name "$( date +"%a %b %d %Y %I:%M %p" )"
-    sleep "$(expr 60 - $( date +"%S" ) )s"
+    DATE="$( date +'%a %b %d %Y %I:%M %p' )"
+    BATTERY="$( cat /sys/class/power_supply/BAT1/capacity )%"
+    xsetroot -name " $DATE -$BATTERY- "
+    sleep 1s
 done &
 
 # start compositor
